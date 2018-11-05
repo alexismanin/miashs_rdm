9ed7a7050e5c4fbe811df2cfc9f37899ff7d4b5b
DelaforgeAlexis <alexis.delaforge@outlook.fr>
2018-11-05 13:56:11 +0100

    Blabla pack, EJS pack
    Ajout packages Blablacar et EJS, modification

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const mongoose = require('mongoose')
 
+// Blablacar variable
+
+import BlablacarClient from "node-blablacar";
+var client_blabla = new BlablacarClient("b98d61a302fd44b2821ff3eadfd17956"); // Blablacar API Key
+
+// Doc Blablacar API https://dev.blablacar.com/docs/versions/1.0/getting-started
+
 const app = express()
 
 // Database client init
@@ -13,9 +20,17 @@ try {
 }
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.render('index.ejs');
+})
+
+.get('/', function (req, res) {
+  res.render('result.ejs');
 })
 
 app.listen(8089, function () {
   console.log('Example app listening on port 8089!')
 })
+
+
+
+
