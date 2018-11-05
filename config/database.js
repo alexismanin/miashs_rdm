31d8d37eed719849f5d6056224e8b800cfe687f5
Alexis Manin <alexis.manin@gmail.com>
2018-11-05 13:16:41 +0100

    chore: add MongoDB support

---
+++ config/database.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'url' : process.env.DB_URL 
+    // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+}
--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
 const app = express()
+
+// Database client init
+const configDB = require('./config/database.js')
+
+try {
+  const db = mongoose.createConnection(configDB.url)
+} catch (error) {
+  console.error("No MongoDB url set. Please provide it using environment variable.");
+}
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
